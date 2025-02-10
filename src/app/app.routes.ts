b1856dda13e4baf891f97a3906611ccb0e35dd4c
MichaelSani05 <msmichaelsani@gmail.com>
2025-02-10 12:10:21 +0100

    fes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,5 +2,6 @@ import { Routes } from '@angular/router';
 import { MainComponent } from './pages/main/main.component';
 
 export const routes: Routes = [
-    {path: '', component: MainComponent}
+    {path: '', component: MainComponent},
+    {path: 'main', component: MainComponent}
 ];
